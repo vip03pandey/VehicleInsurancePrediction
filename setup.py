e78e0d302f5641f6ce2081ed329a26e15a60a5ca
vipul <vipul03pandey@gmail.com>
2025-05-03 16:31:36 +0530

    Fixed _id issue in DataIngestion and updated DataTransformation

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="src",
-    version="0.0.1",
-    author="Vipul Pandey",
-    author_email="vipul03pandey@gmail.com",
-    packages=find_packages()
-)
